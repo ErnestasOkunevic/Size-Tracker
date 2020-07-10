b325d0f38962af70541da4f44ef2415dac8a5bc4
Ernestas Okunevič <ernestas.okunevic@gmail.com>
2020-07-10 17:36:04 +0300

    Created bottom navigation bar

--- app/src/main/java/com/ernokun/sizetracker/activities/MainActivity.java
+++ app/src/main/java/com/ernokun/sizetracker/activities/MainActivity.java
@@ -1,21 +1,63 @@
 package com.ernokun.sizetracker.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.Switch;
 import android.widget.Toast;
 
 import com.ernokun.sizetracker.R;
+import com.ernokun.sizetracker.activities.fragments.AddFragment;
+import com.ernokun.sizetracker.activities.fragments.HomeFragment;
+import com.ernokun.sizetracker.activities.fragments.SettingsFragment;
 import com.ernokun.sizetracker.entities.Weight;
 import com.ernokun.sizetracker.viewmodels.WeightViewModel;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    // Gets data from repository and passes it on to the activity.
     private WeightViewModel weightViewModel;
+
+
+    // The bottom navigation bar.
+    private BottomNavigationView bottomNav;
+
+
+    // On click listener for bottom navigation bar.
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            Fragment selectedFragment = null;
+
+            switch (menuItem.getItemId()) {
+                case R.id.nav_home:
+                    selectedFragment = new HomeFragment();
+                    break;
+                case R.id.nav_add:
+                    selectedFragment = new AddFragment();
+                    break;
+                case R.id.nav_settings:
+                    selectedFragment = new SettingsFragment();
+                    break;
+            }
+
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, selectedFragment)
+                    .commit();
+
+            return true;
+        }
+    };
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,9 +75,26 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onChanged(List<Weight> weights) {
                 // update RecyclerView
-                Toast.makeText(MainActivity.this, "It worked", Toast.LENGTH_LONG).show();
+                Toast.makeText(MainActivity.this, "It worked", Toast.LENGTH_LONG)
+                        .show();
             }
         });
+
+        // Self-explanatory
+        prepareBottomNavigationBar();
+
+    }
+
+    private void prepareBottomNavigationBar() {
+        bottomNav = findViewById(R.id.bottom_navigation_bar_id);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        // Sets the home fragment to be selected when the app opens
+        getSupportFragmentManager().beginTransaction()
+                .replace(R.id.fragment_container, new HomeFragment())
+                .commit();
+
+        bottomNav.getMenu().findItem(R.id.nav_home).setChecked(true);
     }
 
 }
